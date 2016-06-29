66276705a7ce63219b2729cbfa604c5ce1918b68
codingmozart <codingmozart@gmail.com>
2016-06-29 22:14:12 +0800

    subrequest test

--- nginx-1.8.1/src/addon/ngx_http_mytest_module.c
+++ nginx-1.8.1/src/addon/ngx_http_mytest_module.c
@@ -21,6 +21,8 @@ static ngx_int_t mytest_upstream_create_request(ngx_http_request_t *r);
 static ngx_int_t mytest_process_status_line(ngx_http_request_t *r);
 static ngx_int_t mytest_upstream_process_header(ngx_http_request_t *r);
 static void mytest_upstream_finalize_request(ngx_http_request_t *r,ngx_int_t rc);
+static void mytest_post_handler(ngx_http_request_t *r);
+static ngx_int_t mytest_subrequest_post_handler(ngx_http_request_t *r, void *data, ngx_int_t rc);
 
 static ngx_command_t ngx_http_mytest_commands[]={
 
@@ -47,6 +49,11 @@ static ngx_http_module_t ngx_http_mytest_module_ctx = {
 	ngx_http_mytest_create_loc_conf,
 	ngx_http_mytest_merge_loc_conf,
 };
+
+
+typedef struct{
+	ngx_str_t stock[6];
+}ngx_http_mytest_ctx_t;
 
 ngx_module_t ngx_http_mytest_module = {
 	NGX_MODULE_V1,
@@ -93,7 +100,42 @@ ngx_http_mytest_handler(ngx_http_request_t *r)
 {
 
 
+
 	ngx_http_mytest_ctx_t *myctx = ngx_http_get_module_ctx(r,ngx_http_mytest_module);
+	if(myctx == NULL){
+		myctx = ngx_palloc(r ->pool,sizeof(ngx_http_mytest_ctx_t));
+		if(myctx == NULL){
+			return NGX_ERROR;
+		}
+		ngx_http_set_ctx(r,myctx,ngx_http_mytest_module);
+	}
+
+	ngx_http_post_subrequest_t *psr = ngx_palloc(r ->pool,sizeof(ngx_http_post_subrequest_t));
+	if(psr == NULL){
+		return NGX_HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	psr ->handler = mytest_subrequest_post_handler;
+	psr ->data = myctx;
+
+	ngx_str_t sub_prefix = ngx_string("/list=");
+	ngx_str_t sub_location;
+	sub_location.len = sub_prefix.len + r ->args.len;
+	sub_location.data = ngx_palloc(r ->pool,sub_location.len);
+	ngx_snpirntf(sub_location.data,sub_location.len,"%V%V",&sub_prefix,&r ->args);
+
+	ngx_http_request_t *sr;
+	ngx_int_t rc = ngx_http_subrequest(r,&sub_location,NULL,&sr,psr,NGX_HTTP_SUBREQUEST_IN_MEMORY);
+	if(rc != NGX_OK){
+		return NGX_ERROR;
+	}
+
+
+	return NGX_DONE;
+
+
+
+/*	ngx_http_mytest_ctx_t *myctx = ngx_http_get_module_ctx(r,ngx_http_mytest_module);
 	if(myctx == NULL){
 		myctx = ngx_palloc(r ->pool,sizeof(ngx_http_mytest_ctx_t));
 		if(myctx == NULL){
@@ -146,6 +188,14 @@ ngx_http_mytest_handler(ngx_http_request_t *r)
 	ngx_http_upstream_init(r);
 
 	return NGX_DONE;
+*/
+
+
+
+
+
+
+
 
 /*	if(!(r ->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))){
 		return NGX_HTTP_NOT_ALLOWED;
@@ -417,3 +467,76 @@ mytest_upstream_finalize_request(ngx_http_request_t *r,ngx_int_t rc)
 	ngx_log_error(NGX_LOG_DEBUG,r ->connection ->log,0,
 			"mytest_upstream_finalize_request");
 }
+
+
+static ngx_int_t
+mytest_subrequest_post_handler(ngx_http_request_t *r, void *data, ngx_int_t rc)
+{
+	ngx_http_request_t *pr = r ->parent;
+	ngx_http_mytest_ctx_t *myctx = ngx_http_get_module_ctx(pr,ngx_http_mytest_module);
+
+	pr ->headers_out.status = r ->headers_out.status;
+
+	if(r ->headers_out.status == NGX_HTTP_OK){
+		int flag = 0;
+		ngx_buf_t * pRecvBuf = &r ->upstream ->buffer;
+
+		for(;pRecvBuf ->pos != pRecvBUf ->last;pRecvBuf++){
+			if(*pRecvBuf ->pos == ',' || *pRecvBuf ->pos == '\"'){
+				if(flag > 0){
+					myctx ->stock[flag - 1].len = pRecvBuf ->pos - myctx ->stock[flag - 1];
+				}
+				flag++;
+				myctx ->stock[flag - 1].data = pRecvBuf ->pos + 1;
+			}
+
+			if(flag > 6){
+				break;
+			}
+		}
+
+
+		pr ->write_event_handler = mytest_post_handler;
+
+		return NGX_OK;
+	}
+}
+
+
+static void
+mytest_post_handler(ngx_http_request_t *r)
+{
+	if(r ->handlers_out.status != NGX_HTTP_OK){
+		ngx_http_finalize_request(r,r ->headers_out.status);
+		return;
+	}
+
+	ngx_http_mytest_ctx_t *myctx = ngx_http_get_module_ctx(r,ngx_http_mytest_module);
+	
+	ngx_str_t output_format = ngx_string("stock[%V],Today current price: %V, volumn: %V");
+
+	int bodylen = output_format.len + myctx ->stock[0].len +
+		myctx ->stock[1].len + myctx[4].len - 6;
+	r ->headers_out.content_length_n = bodylen;
+
+	ngx_buf_t *b = ngx_create_temp_buf(r ->pool,bodylen);
+	ngx_snprintf(b ->pos,bodylen,(char *)output_format.data,&myctx ->stock[0],&myctx ->stock[1],&myctx ->stock[4]);
+	b ->last = b ->pos + bodylen;
+	b ->last_buf = 1;
+
+	ngx_chain_t out;
+	out.buf = b;
+	out.next = NULL;
+
+	static ngx_str_t type = ngx_string("text/plain; charset=GBK");
+	r ->headers_out.content_type = type;
+	r ->headers_out.status = NGX_HTTP_OK;
+
+	r ->connection ->buffered |= NGX_HTTP_WRITE_BUFFERED;
+	ngx_int_t ret = ngx_http_send_header(r);
+	ret = ngx_http_output_filter(r,&out);
+
+
+	ngx_http_finalize_request(r,ret);
+}
+
